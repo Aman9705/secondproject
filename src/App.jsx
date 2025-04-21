88d2fa00229298d337b274632f6874cf5d8807bf
AMAN <amanbobby7877@gmail.com>
2025-04-21 16:16:13 +0530

    react secod project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import"./App.jsx"
+const App=()=>{
+    return(
+        <div className="first">
+            <h1 className="raju">job</h1>
+            <p className="rani">react based job now a days very populer</p>
+        </div>
+    )
+}
+export default App
